32a4301879c6455bc6b7867195b9b8ff05f69e78
Nathaniel Mom <nathanielmom@gmail.com>
2025-07-21 09:10:52 +1000

    Change tmux pane navigation and update mason lsp config for v2.0.0

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -101,14 +101,16 @@ return {
       ensure_installed = vim.tbl_keys(servers),
     })
 
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-        }
-      end,
+    mason_lspconfig.setup({
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+          }
+        end,
+      }
     })
 
     -- Diagnostic keymaps
